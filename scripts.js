0da13939b42477dfa3fa5343c1e9e1a6935363e2
MarceloPR22 <2marceloribeiro2@gmail.com>
2023-05-04 18:45:41 +0200

    First Site JS

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+function trocaCor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImagem(imagem){
+    let imgIphone = document.querySelector(".imagem-iphones")
+    imgIphone.src = imagem
+}
